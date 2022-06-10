8dd7534fbcb3f550ffde82daeae926f4134f17cb
Vlad <29-vinitskiy-vo@mail.ru>
2022-06-10 22:17:47 +0700

    дз-3 и дз-4

---
+++ react-chat/src/components/message-list/message-list.js
@@ -0,0 +1,114 @@
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import { useParams } from "react-router-dom";
+// import PropTypes from "prop-types";
+import { Input, InputAdornment } from "@mui/material";
+import styled from "@emotion/styled";
+import { Send } from "@mui/icons-material";
+import { Message } from "./message";
+
+const InputStyles = styled(Input)`
+  color: #9a9fa1;
+  padding: 10px 15px;
+  font-size: ${(props) => {
+    return "15px";
+  }};
+`;
+
+const IconStyles = styled(Send)`
+  color: #2b5278;
+`;
+
+const getBotMessage = () => ({
+  author: "Bot",
+  message: "Hello from bot",
+  date: new Date(),
+});
+
+const getBotAnswer = (message) => {
+  const answers = {
+    0: "00000",
+    1: "11111",
+  };
+
+  return answers[message] || "not found answer";
+};
+
+export const MessageList = () => {
+  const { roomId } = useParams();
+
+  const [value, setValue] = useState("");
+  const [messageList, setMessageList] = useState({
+    room1: [getBotMessage()],
+  });
+
+  const ref = useRef();
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollTo(0, ref.current.scrollHeight);
+    }
+  }, [messageList]);
+
+  const sendMessage = useCallback(
+    (message, author = "User") => {
+      if (message) {
+        setMessageList((state) => ({
+          ...state,
+          [roomId]: [
+            ...(state[roomId] ?? []),
+            { author, message, date: new Date() },
+          ],
+        }));
+        setValue("");
+      }
+    },
+    [roomId]
+  );
+
+  useEffect(() => {
+    const messages = messageList[roomId] ?? [];
+    const lastMessage = messages[messages.length - 1];
+    let timerId = null;
+
+    if (messages.length && lastMessage?.author === "User") {
+      timerId = setTimeout(() => {
+        sendMessage(getBotAnswer(lastMessage.message), "Bot");
+      }, 500);
+    }
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [sendMessage, messageList, roomId]);
+
+  const handlePressInput = ({ code }) => {
+    if (code === "Enter") {
+      sendMessage(value);
+    }
+  };
+
+  const messages = messageList[roomId] ?? [];
+
+  return (
+    <>
+      <div ref={ref}>
+        {messages.map((message, index) => (
+          <Message message={message} key={message?.date ?? index} />
+        ))}
+      </div>
+
+      <InputStyles
+        placeholder="enter message ..."
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        onKeyDown={handlePressInput}
+        fullWidth={true}
+        endAdornment={
+          <InputAdornment position="end">
+            {value && <IconStyles onClick={() => sendMessage(value)} />}
+          </InputAdornment>
+        }
+      />
+    </>
+  );
+};
--- react-chat/src/index.js
+++ react-chat/src/index.js
@@ -1,203 +1,33 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-// import { Message } from './components/Message';
-// import { Test } from './components/test/test';
-
-// class ClassComponent extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       count: 0,
-//       films: ["film1", "film2", "film3"],
-//       testObject: {
-//         id: 11,
-//       },
-//     };
-//   }
-
-//   incriment = () => {
-//     this.setState({
-//       count: this.state.count + 1,
-//     });
-//   };
-
-//   decriment = () => {
-//     this.setState({
-//       count: this.state.count - 1,
-//     });
-//   };
-
-//   addFilm = () => {
-//     const film = 'new film'
-//     this.setState({
-//       films:[...this.state.films, film]
-//     })
-//   };
-
-//   render() {
-//     console.log(this.state);
-//     const { count, films, testObject } = this.state;
-//     return (
-//       <div>
-//         <button onClick={this.incriment}>Incriment</button>
-//         <button onClick={this.decriment}>Decriment</button>
-//         <button onClick={this.addFilm}>addFilm</button>
-//         <div>
-//           <h1>ClassComponent</h1>
-//           <h1>
-//             Count: <span>{count}</span>
-//           </h1>
-//         </div>
-//         <hr></hr>
-//         <div>
-//           <h1>Фильмы: </h1>
-//           <div>
-//             {films.map((film) => (
-//               <h1>{film}</h1>
-//             ))}
-//           </div>
-//         </div>
-//         <hr></hr>
-//         <div>
-//           <h1>testObject: </h1>
-//           <div>
-//             <h1>Свойство testObject: {testObject.id}</h1>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-const FunctionComponent = () => {
-  const [count, setCount] = useState(0);
-  const [films, setFilms] = useState(["Film 1", "Film 2", "Film 3"]);
-  const [testObject, setTestObject] = useState({
-    id: 1988,
-  });
-
-  useEffect(() => {
-    console.log("Эффект1");
-  }, []);
-
-  useEffect(() => {
-    console.log("Эффект3: count", count);
-  }, [count]);
-
-  useEffect(() => {
-    console.log("Эффект4: Фильмы", films);
-  }, [films]);
-
-  const addFilm = () => {
-    const film = "Новый фильм";
-    setFilms([...films, film]);
-  };
-
-  const showTestObject = () => {
-    setTestObject();
-    console.log(testObject);
-  };
-
-  return (
-    <div>
-      <h1>FunctionComponent</h1>
-      <hr></hr>
-      <div>
-        <h1>Count: {count}</h1>
-        <button onClick={() => setCount(count + 1)}>incriment</button>
-      </div>
-      <hr></hr>
-      <div>
-        <h1>Фильмы:</h1>
-        <button onClick={addFilm}>Добавить фильм</button>
-        <div>
-          {films.map((film) => (
-            <h1>{film}</h1>
-          ))}
-        </div>
-      </div>
-      <hr></hr>
-      <div>
-        <h1>{testObject.id}</h1>
-      </div>
-    </div>
-  );
-};
-
-const MassageList = () => {
-  const BOT_MESSAGE = { autor: "Таня", massage: "Я Май" };
-  const ref = useRef();
-  const [value, setValue] = useState("");
-  const [massages, setMassages] = useState([BOT_MESSAGE]);
-
-  useEffect(() => {
-    ref.current?.focus()
-  });
-
-  useEffect(() => {
-    console.log(ref)
-    const lastMessage = massages[massages.length - 1];
-    let timerId = null;
-
-    if (massages.length && lastMessage?.autor == "Влад") {
-      timerId = setTimeout(() => {
-        setMassages([...massages, BOT_MESSAGE]);
-      }, 5000);
-    }
-    // setMassages([...massages, BOT_MESSAGE]);
-    return () => {
-      clearInterval(timerId);
-    };
-  }, [massages]);
-
-  const sendMassage = () => {
-    if (value) {
-      setMassages([...massages, { autor: "Влад", massage: value }]);
-    }
-  };
-
-  return (
-    <div>
-      <h1>MassageList</h1>
-      <input
-        ref={ref}
-        placeholder="Введите сообщение"
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
-      />
-      <button onClick={sendMassage}>Send</button>
-      <hr />
-      {massages.map((massage) => (
-        <div>
-          <h2>{massage.autor}</h2>
-          <h2>{massage.massage}</h2>
-          <hr />
-        </div>
-      ))}
-    </div>
-  );
-};
+import { ThemeProvider, createTheme } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./components";
+import { ProfilePage, ChatPage } from "./pages";
+import "./global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <MassageList />
-  // <FunctionComponent />
-  // <React.StrictMode>
+const theme = createTheme({
+  myPalette: {
+    color: "red",
+  },
+  palette: {},
+});
 
-  //   {/* <ClassComponent /> */}
-  //   {/* <Test props1={23}
-  //     props2="5"
-  //     props3={false}
-  //     props4={(arg) => console.log("click", arg)}
-  //     props5={[1, 2, 3]}
-  //     films={[
-  //       { title: 'Witcher', year: 2003 },
-  //       { title: 'Witcher-2', year: 2015 }
-  //     ]}
-  //   /> */}
-  //   {/* <Message message="Соообщение из ДЗ №1" /> */}
-  // </React.StrictMode>
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/chat/*" element={<ChatPage />} />
+          <Route path="/" element={<ProfilePage />} />
+          <Route path="*" element={<h1>404</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
 );
